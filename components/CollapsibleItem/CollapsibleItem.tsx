441c5c6bccee1abe6a63384263541a38f9b0e2db
Andy Lau <landywl4@gmail.com>
2022-03-03 19:53:11 -0800

    Heroku change

--- components/CollapsibleItem/CollapsibleItem.tsx
+++ components/CollapsibleItem/CollapsibleItem.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, useState, Children, Key } from "react";
+import React, { ReactNode, useState, Children, Key } from "react";
 import styles from "./CollapsibleItem.module.scss";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 interface Props {
   parentNode: ReactNode;
@@ -13,9 +13,16 @@ export default function CollapsibleItem(props: Props) {
 
   const toggleCollapsibleSection = () => {
     setShowCollapsible(!showCollapsible);
-  }
+  };
 
-  const mappedChildrenToList = (Children.count(children) > 1) ? children!.map((item: ReactNode, index: number) => <li key={index}>item</li>) : <li>{children}</li>
+  const mappedChildrenToList =
+    Children.count(children) > 1 ? (
+      React.Children.map(children, (item, index) => {
+        <li key={index}>{item}</li>;
+      })
+    ) : (
+      <li>{children}</li>
+    );
 
   return (
     <div className={styles.collapsibleContainer}>
@@ -23,10 +30,9 @@ export default function CollapsibleItem(props: Props) {
         {parentNode}
         <KeyboardArrowDownIcon />
       </div>
-      {showCollapsible ? <ul className={styles.collapsedNodes}>
-        {mappedChildrenToList}
-      </ul> : null
-      }
+      {showCollapsible ? (
+        <ul className={styles.collapsedNodes}>{mappedChildrenToList}</ul>
+      ) : null}
     </div>
-  )
+  );
 }
