8c7e2209c90ffab4e2a46073d85c85a91c96ddf0
Andy Lau <landywl4@gmail.com>
2022-03-04 19:22:07 -0800

    Login Form Changes

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -1,19 +1,23 @@
 import styles from "./LoginForm.module.scss";
 import FormContainer from "../Form/FormContainer";
 import React, { FormEvent, useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function LoginForm() {
   const formRef = useRef<HTMLInputElement>(null);
   const [formState, setFormState] = useState({ username: "", password: "" });
+  const router = useRouter();
 
   useEffect(() => {
-    if (formRef.current) {
-      formRef.current.focus();
-    }
+    if (formRef.current) formRef.current.focus();
   }, []);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    const response = 200;
+    if(response === 200) {
+      router.push("/shop")
+    }
   };
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
