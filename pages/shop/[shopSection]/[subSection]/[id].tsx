097ccfd13457db7122b89e36549028ceb6bd3128
Andy Lau <landywl4@gmail.com>
2022-03-11 22:49:42 -0800

    Added item page

--- pages/shop/[shopSection]/[subSection]/[id].tsx
+++ pages/shop/[shopSection]/[subSection]/[id].tsx
@@ -1,9 +1,13 @@
 import Header from "../../../../components/Header/Header";
-import { getAllItems, getAllSections, getAllItemPaths } from "../../../../TestData/SectionItems";
+import {
+  getAllItems,
+  getAllItemPaths,
+} from "../../../../TestData/SectionItems";
+import styles from "../../../../styles/Item.module.scss";
+import Image from "next/image";
 
 export const getStaticPaths = async () => {
   const paths = getAllItemPaths();
-  console.log(paths)
   return {
     paths,
     fallback: false,
@@ -14,23 +18,52 @@ interface Params {
   params: {
     shopSection: string;
     subSection: string;
+    id: number;
   };
 }
 
 export const getStaticProps = async ({ params }: Params) => {
+  const item = getAllItems().filter(
+    (item: { id: any }) => item.id == params.id
+  )[0];
   return {
     props: {
+      details: item,
     },
   };
 };
 
-const Item = () => {
-  return (
-    <div>
-      <Header />
-      <p>Test</p>
-    </div>
-  )
+interface Props {
+  details: {
+    id: number;
+    name: string;
+    description: string;
+    price: string;
+    image: string;
+  };
 }
+
+const Item = (props: Props) => {
+  const { details } = props;
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <h1>{details.name}</h1>
+        <div className={styles.imageContainer}>
+          <Image
+            src={details.image}
+            alt={details.name}
+            layout="responsive"
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <p>{details.description}</p>
+        <p>{details.price}</p>
+      </main>
+    </>
+  );
+};
 
 export default Item;
