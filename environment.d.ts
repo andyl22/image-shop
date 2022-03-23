66ae1015c445c7fd7b5d7311c0dc6329a12d139c
Andy Lau <landywl4@gmail.com>
2022-03-22 23:39:50 -0700

    Fixed capitlization error

--- environment.d.ts
+++ environment.d.ts
@@ -2,8 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
-      MONGO_URI: String;
-      SECRET: String;
+      MONGO_URI: string;
     }
   }
 }
