dd1c161b1411e5a5bb43533302e72569c72a603d
Andy Lau <landywl4@gmail.com>
2022-04-18 12:59:17 -0700

    Auth fix

--- components/AuthGuard/AuthGuard.tsx
+++ components/AuthGuard/AuthGuard.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { selectUser } from "../../redux/slices/userSlice";
 
@@ -10,24 +11,26 @@ export default function AuthGuard(props: Props) {
   const router = useRouter();
   const user = useAppSelector(selectUser);
   const { children } = props;
-  const href = window.location.pathname;
 
-  if (user.user.username) {
-    const invalidURLs = ["/user/register", "/user/login"];
+  useEffect(() => {
+    const href = window.location.pathname;
+    if (user.user.username) {
+      const invalidURLs = ["/user/register", "/user/login"];
 
-    if (invalidURLs.includes(href)) {
-      router.push("/");
+      if (invalidURLs.includes(href)) {
+        router.push("/");
+      }
+    } else {
+      const protectedURLs = [
+        "/user/preferences",
+        "/user/orders",
+        "/user/account",
+      ];
+      if (protectedURLs.includes(href)) {
+        router.push("/");
+      }
     }
-  } else {
-    const protectedURLs = [
-      "/user/preferences",
-      "/user/orders",
-      "/user/account",
-    ];
-    if (protectedURLs.includes(href)) {
-      router.push("/");
-    }
-  }
+  });
 
   return <>{children}</>;
 }
