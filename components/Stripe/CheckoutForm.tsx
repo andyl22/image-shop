04a1609128c689fe5fd8459a81fcec4d095e6be4
Andy Lau <landywl4@gmail.com>
2022-04-18 19:05:32 -0700

    redirect link change for dev/prod

--- components/Stripe/CheckoutForm.tsx
+++ components/Stripe/CheckoutForm.tsx
@@ -61,7 +61,10 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/shop/checkout",
+        return_url:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3000/shop/checkout"
+            : "https://parks-shop.herokuapp.com/shop/checkout",
       },
     });
 
